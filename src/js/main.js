1dd8ef2243f6d054e273cf5918161f27d5949fdb
Patrick Hurley <pathurley7@gmail.com>
2021-03-01 17:15:53 +0000

    Added functionality for nat-default, nat-column, and nat-row. Needs refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,32 @@
 (function () {
 
-    const orgTable = document.querySelector('[nat]');
-    const tableHeader = orgTable.querySelector('thead');
-    const tableBody = orgTable.querySelector('tbody')
+    let orgTable;
+    let tableHeader;
+    let tableHeaderItems;
+    let tableBody;
+    let tableType;
+    let natTable;
 
-    // start building the nat
-    let natTable = '<div class="nat__table">';
+    function initNAT(){
+        orgTable = document.querySelector('[nat]');
+        tableHeader = orgTable.querySelector('thead');
+        tableBody = orgTable.querySelector('tbody');
+        tableType = 'nat-default';
+        if(document.body.contains(document.querySelector('[nat-column]'))) {
+            tableType = 'nat-column';
+        }
+        else if(document.body.contains(document.querySelector('[nat-row]'))) {
+            tableType = 'nat-row';
+        }
+        // start building the nat
+        natTable = `<div class="nat__table ${tableType}">`;
+    }
 
     function buildHeader() {
         // start building table header
         if (tableHeader) {
             // find all header items
-            const tableHeaderItems = tableHeader.querySelectorAll('th');
+            tableHeaderItems = tableHeader.querySelectorAll('th');
 
             if (tableHeaderItems) {
                 natTable += '<div class="nat__row-group" role="rowgroup">'
@@ -27,40 +42,115 @@
                 natTable += '</div></div>';
             }
 
-            buildBody(tableHeaderItems)
-
         } else {
-            console.error('NAT: No thead found');
-            return
+            if(tableType !== 'nat-row'){
+                console.error('NAT: No thead found');
+                return
+            }
         }
     }
 
-    function buildBody(tableHeaderItems) {
+    function buildBody() {
 
         // start building table body
         if (tableBody) {
 
             // find all body rows
-            const tableBodyRows = orgTable.querySelectorAll('tbody tr')
+            let tableBodyRows = orgTable.querySelectorAll('tbody tr')
             if (tableBodyRows) {
 
                 natTable += '<div class="nat__row-group" role="rowgroup">';
 
-                // for each body row
-                tableBodyRows.forEach(row => {
-                    natTable += '<div class="nat__row" role="row">';
+                // ! NAT COLUMN
+                if(tableType === 'nat-column') {
 
-                    // find row items
-                    let rowItems = row.querySelectorAll('td');
-                    rowItems.forEach((item, index) => {
-                        natTable += `<div class="nat__cell" role="cell">`;
-                        natTable +=
-                            `<span class="nat__visible--xs">${tableHeaderItems.item(index).textContent}</span>${item.textContent}</div>`;
+                    tableHeaderItems.forEach((headerItem, index) => {
+                        
+                        natTable += '<div class="nat__row" role="row">';
+                        natTable += `<div class="nat__cell nat__visible--xs" role="cell">${headerItem.textContent}</div>`;
+
+                        tableBodyRows.forEach((row) => {
+                            let rowItem = row.querySelector(`td:nth-of-type(${index+1})`);
+                            natTable += `<div class="nat__cell nat__visible--xs" role="cell">${rowItem.textContent}</div>`;
+                            
+                        });
+
+
+                        natTable += '</div>';
                     });
 
-                    // close the row
-                    natTable += '</div>';
-                });
+                    // for each body row
+                    tableBodyRows.forEach(row => {
+                        natTable += '<div class="nat__row nat__hidden--xs" role="row">';
+
+                        // find row items
+                        let rowItems = row.querySelectorAll('td');
+                        rowItems.forEach((item, index) => {
+                            natTable += `<div class="nat__cell" role="cell">`;
+                            natTable += `<span>${item.textContent}</span>`;
+                            natTable += `</div>`;
+                        });
+
+                        // close the row
+                        natTable += '</div>';
+                    });
+
+                }
+
+                // ! NAT DEFAULT
+                else if(tableType === 'nat-row') {
+                    // for each body row
+                    tableBodyRows.forEach(row => {
+                        natTable += '<div class="nat__row" role="row">';
+
+                        // find row items
+                        let rowItems = row.querySelectorAll('td');
+                        rowItems.forEach((item, index) => {
+                            
+                            if(index === 0){
+                                natTable += `<div class="nat__cell" role="columnheader">`;  
+                            }
+                            else {
+                                natTable += `<div class="nat__cell" role="cell">`;
+                            }
+                            
+                            natTable += `<span>${item.textContent}</span>`;
+                            natTable += `</div>`;
+                        });
+
+                        // close the row
+                        natTable += '</div>';
+                    });
+                }
+
+
+                // ! NAT DEFAULT
+                else {
+                    // for each body row
+                    tableBodyRows.forEach(row => {
+                        natTable += '<div class="nat__row" role="row">';
+
+                        // find row items
+                        let rowItems = row.querySelectorAll('td');
+                        rowItems.forEach((item, index) => {
+                            if(tableHeaderItems.item(index).textContent !== ''){
+                                natTable += `<div class="nat__cell" role="cell">`;
+                                natTable += `<span class="nat__visible--xs">${tableHeaderItems.item(index).textContent}: </span>`;
+                            }
+                            else {
+                                natTable += `<div class="nat__cell" role="columnheader">`;  
+                            }
+                            natTable += `<span>${item.textContent}</span>`;
+                            natTable += `</div>`;
+                        });
+
+                        // close the row
+                        natTable += '</div>';
+                    });
+                }
+
+                // close the row group
+                natTable += '</div>';
             }
 
         } else {
@@ -69,8 +159,10 @@
         }
     }
 
-    buildHeader();
-
-    orgTable.outerHTML = natTable;
-
+    if(document.body.contains(document.querySelector('[nat'))) {
+        initNAT();
+        if(buildHeader() !== 'error') { buildBody() } 
+        orgTable.outerHTML = natTable;
+    }
+    
 })();
