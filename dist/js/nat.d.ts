6077613d352971886cee0bcc37051fa231cd5f49
Patrick Hurley <pathurley7@gmail.com>
2021-03-15 13:54:06 +0000

    Corrected nat.d.ts declaration to return an object for convert()

--- dist/js/nat.d.ts
+++ dist/js/nat.d.ts
@@ -3,7 +3,7 @@ declare function nat(): void;
 declare namespace nat {  
 
   export function init(): void;
-  export function convert(inp?: string): string;
+  export function convert(inp?: string): { display: string; error: string; };
 
 }
 
