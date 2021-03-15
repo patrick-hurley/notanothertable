325e069b7d658d2b62756134ac905f6f4e7ffa74
Patrick Hurley <pathurley7@gmail.com>
2021-03-15 13:40:29 +0000

    Updated nat.d.ts function declaration

--- dist/js/nat.d.ts
+++ dist/js/nat.d.ts
@@ -1,1 +1,10 @@
-declare module 'notanothertable'
+declare function nat(): void;
+
+declare namespace nat {  
+
+  export function init(): void;
+  export function convert(inp?: string): string;
+
+}
+
+export = nat;
